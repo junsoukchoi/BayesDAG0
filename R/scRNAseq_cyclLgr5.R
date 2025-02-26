c500fc2751374f89326ce9e3db69058db4ab2b6a
Junsouk Choi <jchoi@stat.tamu.edu>
2025-02-26 22:36:39 +0900

    Add the differential AhR scRNA-seq dataset

---
+++ R/scRNAseq_cyclLgr5.R
@@ -0,0 +1,11 @@
+#' Differential AhR scRNA-seq dataset
+#'
+#' A dataset of single-cell RNA-sequecning read counts for 42 Wnt signaling pathway genes in cycling Lgr5+ colonic stem cells from AhR wild type and knockout mice.
+#'
+#' @format A list with two elements:
+#' \describe{
+#'   \item{WT}{A matrix of single-cell RNA-sequencing data for the AhR wild-type condition.}
+#'   \item{KO}{A matrix of single-cell RNA-sequencing data for the AhR knockout condition.}
+#' }
+#' @source 
+"scRNAseq_cyclLgr5"
